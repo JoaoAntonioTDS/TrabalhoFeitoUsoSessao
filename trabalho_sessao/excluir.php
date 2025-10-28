6737122b64f419dfe2c74d569b0b99d99709d415
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2025-10-28 19:24:36 -0300

    Enhance session handling in excluir.php
    Redirect to mostrar.php with error message if 'contagem' is not set.

--- trabalho_sessao/excluir.php
+++ trabalho_sessao/excluir.php
@@ -4,8 +4,15 @@ session_start();
 if (isset($_SESSION['contagem'])) {
     session_unset();
     session_destroy();
+
+    header("location: mostrar.php");
+exit;
+
+} else{
+    $erro = "Contagem já foi apagada";
+    header("location: mostrar.php?erro=" . $erro);
+    
+    exit;
 }
 
-header("location: mostrar.php");
-    
-exit;
+
